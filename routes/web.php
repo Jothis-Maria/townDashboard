de330f0ab709cf2771a17249c4b941af985e49c4
Jothis-Maria <emailjothis@gmail.com>
2023-10-12 13:20:48 +0530

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/demo/{name}/{id?}',function ($name, $id = null){
+    echo $name;
+    echo $id;
+    
+});
+
+
+Route::any('/test',function(){
+    echo"testing the route";
+});
